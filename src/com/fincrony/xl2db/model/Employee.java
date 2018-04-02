32f49ea8eb2bed8eae34e2a300dbbbb61674914f
prabhu052 <37905766+prabhu052@users.noreply.github.com>
2018-04-02 20:36:36 +0530

    test

--- src/com/fincrony/xl2db/model/Employee.java
+++ src/com/fincrony/xl2db/model/Employee.java
@@ -1,7 +1,7 @@
 package com.fincrony.xl2db.model;
 
 public class Employee {
-	
+
 	private String id;
 	private String name;
 	private String address;
@@ -11,32 +11,41 @@ public class Employee {
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public Double getSalary() {
 		return salary;
 	}
+
 	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
+
 	public String getJoiningDate() {
 		return joiningDate;
 	}
+
 	public void setJoiningDate(String joiningDate) {
 		this.joiningDate = joiningDate;
 	}
-	
+
 }
