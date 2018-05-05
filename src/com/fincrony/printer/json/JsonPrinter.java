20a81d137a32b6a711c02a7f211dcbac25f11a13
prabhu052 <37905766+prabhu052@users.noreply.github.com>
2018-05-05 22:39:14 +0530

    hi

--- src/com/fincrony/printer/json/JsonPrinter.java
+++ src/com/fincrony/printer/json/JsonPrinter.java
@@ -1,40 +1,38 @@
 package com.fincrony.printer.json;
 
-import java.util.Scanner;
-
 import org.codehaus.jackson.map.ObjectMapper;
 import com.fincrony.printer.Printer;
 import com.fincrony.xl2db.model.Employee;
-import com.fincrony.xl2db.parser.DefaultEmployeeParser;
 
 public class JsonPrinter implements Printer {
 
+	//@SuppressWarnings("resource")
 	@Override
 	public void jPrint() {
 		// TODO Auto-generated method stub
 		ObjectMapper mapper = new ObjectMapper();	
 		
-		 /*Employee emp = new Employee();
+		 Employee emp = new Employee();
 		 
-		    emp.setId("1");
+		    emp.setSl("1");
 			emp.setName("Prabhu");
 			emp.setAddress("bbsr");
 			emp.setSalary(42000.00);
-			emp.setJoiningDate("doj");*/
+			emp.setJoiningDate("doj");
 		
-		System.out.println("Enter the path of the Excel :");
+		/*System.out.println("Enter the path of the Excel :");
 		Scanner sc = new Scanner(System.in);
 		String path = sc.next();	
-		
-		
+				
 		new DefaultEmployeeParser().readExcel(path);
 		
 		Employee emp = new Employee();
+		
 		emp.getSl();
 		emp.getName();
 		emp.getAddress();
 		emp.getSalary();
-		emp.getJoiningDate();
+		emp.getJoiningDate();*/
 		
 				try {
 					System.out.println(mapper
